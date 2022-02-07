e16240eb37a618105b39b5fd15a5877878f8f7fb
Wasinthorn Suksri <mccommander_minecraft@hotmail.com>
2022-02-07 19:05:39 +0700

    1.0.0 Release

--- src/main/java/com/stevekung/unseencommands/MixinDebugPackets.java
+++ src/main/java/com/stevekung/unseencommands/MixinDebugPackets.java
@@ -21,69 +21,6 @@ public class MixinDebugPackets
     @Shadow
     static void sendPacketToAllPlayers(ServerLevel serverLevel, FriendlyByteBuf friendlyByteBuf, ResourceLocation resourceLocation) {}
 
-    //    @Overwrite
-    //    public static void sendNeighborsUpdatePacket(Level level, BlockPos pos)
-    //    {
-    //        var friendlyByteBuf = new FriendlyByteBuf(Unpooled.buffer());
-    //
-    //        friendlyByteBuf.writeVarLong(level.getGameTime());
-    //        friendlyByteBuf.writeBlockPos(pos);
-    //
-    //        sendPacketToAllPlayers((ServerLevel) level, friendlyByteBuf, ClientboundCustomPayloadPacket.DEBUG_NEIGHBORSUPDATE_PACKET);
-    //    }
-
-    //    @Overwrite
-    //    public static void sendGameEventInfo(Level level, GameEvent gameEvent, BlockPos pos)
-    //    {
-    //        var friendlyByteBuf = new FriendlyByteBuf(Unpooled.buffer());
-    //
-    //        friendlyByteBuf.writeUtf(gameEvent.getName());
-    //        friendlyByteBuf.writeBlockPos(pos);
-    //
-    //        sendPacketToAllPlayers((ServerLevel) level, friendlyByteBuf, ClientboundCustomPayloadPacket.DEBUG_GAME_EVENT);
-    //    }
-
-    //    // Not working
-    //    @Overwrite
-    //    public static void sendStructurePacket(WorldGenLevel level, StructureStart<?> structureStart)
-    //    {
-    //        var minecraft = Minecraft.getInstance();
-    //
-    //        if (minecraft.getConnection() == null || minecraft.level == null)
-    //        {
-    //            return;
-    //        }
-    //
-    //        var friendlyByteBuf = new FriendlyByteBuf(Unpooled.buffer());
-    //        var bb = structureStart.getBoundingBox();
-    //        var size = structureStart.getPieces().size();
-    //
-    //        friendlyByteBuf.writeResourceLocation(DimensionType.OVERWORLD_LOCATION.location());
-    //        friendlyByteBuf.writeInt(bb.minX());
-    //        friendlyByteBuf.writeInt(bb.minY());
-    //        friendlyByteBuf.writeInt(bb.minZ());
-    //        friendlyByteBuf.writeInt(bb.maxX());
-    //        friendlyByteBuf.writeInt(bb.maxY());
-    //        friendlyByteBuf.writeInt(bb.maxZ());
-    //
-    //        friendlyByteBuf.writeInt(size);
-    //
-    //        for (var piece : structureStart.getPieces())
-    //        {
-    //            var bb2 = piece.getBoundingBox();
-    //            friendlyByteBuf.writeInt(bb2.minX());
-    //            friendlyByteBuf.writeInt(bb2.minY());
-    //            friendlyByteBuf.writeInt(bb2.minZ());
-    //            friendlyByteBuf.writeInt(bb2.maxX());
-    //            friendlyByteBuf.writeInt(bb2.maxY());
-    //            friendlyByteBuf.writeInt(bb2.maxZ());
-    //            friendlyByteBuf.writeBoolean(true);
-    //        }
-    //
-    //        var packet = new ClientboundCustomPayloadPacket(ClientboundCustomPayloadPacket.DEBUG_STRUCTURES_PACKET, friendlyByteBuf);
-    //        minecraft.execute(() -> packet.handle(minecraft.getConnection()));
-    //    }
-
     /**
      * @reason Make path debugging work
      * @author SteveKunG
--- src/main/java/com/stevekung/unseencommands/client/MixinDebugRenderer.java
+++ src/main/java/com/stevekung/unseencommands/client/MixinDebugRenderer.java
@@ -10,41 +10,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import com.mojang.blaze3d.vertex.PoseStack;
 
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.debug.*;
-import net.minecraft.client.renderer.debug.DebugRenderer.SimpleDebugRenderer;
+import net.minecraft.client.renderer.debug.DebugRenderer;
+import net.minecraft.client.renderer.debug.PathfindingRenderer;
 
 @Mixin(DebugRenderer.class)
 public class MixinDebugRenderer
 {
-    @Shadow @Final PathfindingRenderer pathfindingRenderer;
-    @Shadow @Final SimpleDebugRenderer waterDebugRenderer;
-    @Shadow @Final SimpleDebugRenderer heightMapRenderer;
-    @Shadow @Final SimpleDebugRenderer collisionBoxRenderer;
-    @Shadow @Final SimpleDebugRenderer neighborsUpdateRenderer;
-    @Shadow @Final StructureRenderer structureRenderer;
-    @Shadow @Final SimpleDebugRenderer lightDebugRenderer;
-    @Shadow @Final SimpleDebugRenderer worldGenAttemptRenderer;
-    @Shadow @Final SimpleDebugRenderer solidFaceRenderer;
-    @Shadow @Final SimpleDebugRenderer chunkRenderer;
-    @Shadow @Final BrainDebugRenderer brainDebugRenderer;
-    @Shadow @Final VillageSectionsDebugRenderer villageSectionsDebugRenderer;
-    @Shadow @Final BeeDebugRenderer beeDebugRenderer;
-    @Shadow @Final RaidDebugRenderer raidDebugRenderer;
-    @Shadow @Final GoalSelectorDebugRenderer goalSelectorRenderer;
-    @Shadow @Final GameEventListenerRenderer gameEventListenerRenderer;
+    @Shadow
+    @Final
+    PathfindingRenderer pathfindingRenderer;
 
     @Inject(method = "render", at = @At("TAIL"))
     private void render(PoseStack poseStack, MultiBufferSource.BufferSource bufferSource, double camX, double camY, double camZ, CallbackInfo info)
     {
-        this.pathfindingRenderer.render(poseStack, bufferSource, camX, camY, camZ);
-        //        this.waterDebugRenderer.render(poseStack, bufferSource, camX, camY, camZ);
-        //        this.heightMapRenderer.render(poseStack, bufferSource, camX, camY, camZ);
-        //        this.collisionBoxRenderer.render(poseStack, bufferSource, camX, camY, camZ);
-        //        this.neighborsUpdateRenderer.render(poseStack, bufferSource, camX, camY, camZ);
-        //        this.structureRenderer.render(poseStack, bufferSource, camX, camY, camZ);
-        //        this.lightDebugRenderer.render(poseStack, bufferSource, camX, camY, camZ);
-        //        this.solidFaceRenderer.render(poseStack, bufferSource, camX, camY, camZ);
-        //        this.chunkRenderer.render(poseStack, bufferSource, camX, camY, camZ);
-        //        this.gameEventListenerRenderer.render(poseStack, bufferSource, camX, camY, camZ);
+        //this.pathfindingRenderer.render(poseStack, bufferSource, camX, camY, camZ);
     }
 }
